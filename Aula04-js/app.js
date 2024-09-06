418d313542de8506a276b4f2c6dd6735c8dca6f0
SabrinaSilva1 <142277089+SabrinaSilva1@users.noreply.github.com>
2024-09-06 10:54:17 -0300

    Add files via upload

---
+++ Aula04-js/app.js
@@ -0,0 +1,89 @@
+
+console.log("Olá Mundo!")
+
+let nome = "Francisco Douglas"
+let idade = 20
+let altura = 1.75
+let vazio //indefinida, variavel vazia seria do tipo null
+
+
+let objeto = {
+    "nome": "Sabrina",
+    "idade": 12,
+    "altura": 1.75
+}
+
+let objeto2 = {
+    nome,
+    idade,
+    altura
+}
+
+console.log(nome, idade, altura, null, true, false, vazio)
+console.log(objeto)
+console.log(objeto2)
+
+//Usando Colchetes [ ]
+let modelos = ['Gol', 'Corsa', 'Fusca'];
+
+//Usando o construtor Array ( )
+let marcas = Array('Chevrolet', 'Ford', 'Fiat');
+
+//Definindo um valor fixo atraves da Array
+let acessorios = new Array(5);
+
+//Criando um vetor vazio 
+let cores = [];
+
+// OU
+let categorias = new Array()
+
+/*
+console.log(modelo)
+console.log(marcas)
+console.log(acessorios)
+console.log(cores)
+
+*/
+
+
+//Exibindo ou recuperando o elemento vetor
+console.log(modelos[0])
+console.log(modelos[1])
+console.log(modelos[2])
+
+let nome_modelo = modelos[0];
+console.log(nome_modelo)
+
+//Adicionando elementos no fim da array
+modelos.push('HB20')
+console.log(modelos)
+
+//Adicionando elementos no inicio da array
+modelos.unshift('Logan')
+
+//Removendo elementos no fim da array
+modelos.shift()
+
+//Remeovendo elementos no inicio da array
+modelos.pop()
+console.log(modelos)
+
+let frutas = ['Laranja', 'Melancia', 'Morango']
+let vegetais = ['Batata Doce', 'Cenoura', 'Milho']
+let feira = frutas.concat(vegetais);
+
+// Exibindo o Vetor
+console.log(feira);
+
+let frutas_ = ['Laranja', 'Melancia', 'Morango', 'Uva', 'Caju']
+let frutas_sel = frutas.slice(1,3) //Slice pega uma cópia, como se fosse metade da array
+
+console.log(frutas_sel)
+
+//Alterando ou removendo elemento de array
+//frutas_.splice(1,1,'Manga')
+
+//console.log(frutas_sel)
+
+
